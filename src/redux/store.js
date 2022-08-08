554dfa90a5f1984ddf21310e0a5fe8e1fac36e3a
Sugam Karki <sugamkarki7058@gmail.com>
2022-08-08 11:13:59 +0545

    redux added

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { itemsSlice } from "./slices/itemsSlice";
+export const store = configureStore({
+  reducer: {
+    items: itemsSlice,
+  },
+});
