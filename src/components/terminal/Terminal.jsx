cf21478d613336acfff98a5ba3d7b019d8df45c6
Sugam Karki <sugamkarki7058@gmail.com>
2022-08-10 07:51:55 +0545

    terminal added

--- src/components/terminal/Terminal.jsx
+++ src/components/terminal/Terminal.jsx
@@ -2,7 +2,45 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleVisibility } from "../../redux/slices/itemsSlice";
 import WinboxComponent from "../../components/winbox/WinboxComponent";
-import { ReactTerminal } from "react-terminal";
+import Terminal from "terminal-in-react";
+function TerminalComponents() {
+  const commands = {
+    linkedin: () =>
+      window.open("https://www.linkedin.com/in/sugam-karki-4877261a3/"),
+    github: () => window.open("https://github.com/sugamkarki"),
+    whoshotfirst: () => "Han Solo shot first",
+    ping: () => "pong",
+    hotel: () => "trivago",
+    whoami: () => "sugamkarki",
+    ls: () => "about, projects, contact",
+    contact: () => "You can contact me on linkedin",
+    help: () => {
+      let keys = Object.keys(commands);
+      keys = keys.map((key) => {
+        return key + "\n";
+      });
+      return keys;
+    },
+  };
+  return (
+    <>
+      <Terminal
+        commands={commands}
+        backgroundColor="#845EC2"
+        color="#FFC75F"
+        style={{ fontWeight: "bold", fontSize: "1em" }}
+        descriptions={{
+          "open-google": "opens google.com",
+          showmsg: "shows a message",
+          alert: "alert",
+          popup: "alert",
+        }}
+        msg="You can write anything here. Example - Hello! My name is Foo and I like Bar."
+        startState="maximised"
+      />
+    </>
+  );
+}
 function TerminalComponent({ onClose }) {
   const { showTerminal } = useSelector((state) => state.items);
   const dispatch = useDispatch();
@@ -15,7 +53,7 @@ function TerminalComponent({ onClose }) {
   };
   return (
     <WinboxComponent title="Terminal" onCloseWindow={() => toggle}>
-      <ReactTerminal commands={commands} />
+      <TerminalComponents />
     </WinboxComponent>
   );
 }
